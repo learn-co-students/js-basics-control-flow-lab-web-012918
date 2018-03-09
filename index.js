c953079bdd6b2a3a328a4c9c1e6a9ba9896ac4dc
allinthemind66 <r.nilon92@gmail.com>
2018-03-09 17:02:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code in this file!
+function scuberGreetingForFeet(ride) {
+  if(ride < 2500)
+    if (ride <= 400){
+      return "This one is on me!"
+    }
+    else{
+      return 'I will gladly take your thirty bucks.'
+    }
+    else{
+      return "No can do."
+    }
+}
+
+function ternaryCheckCity(city) {
+  if(city === "NYC"){
+    return "Ok, sounds good."
+  }
+  else{
+    return "No go."
+  }
+}
+
+function switchOnCharmFromTip(tip){
+  if(tip === 'generous'){
+    return "Thank you so much."
+  }
+  else if (tip === 'not as generous') {
+    return 'Thank you.'
+  }
+  else{
+    return 'Bye.'
+  }
+
+}
