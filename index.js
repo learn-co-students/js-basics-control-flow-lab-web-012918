7fb64d150e6d38e24bc29832cfabe7f0150123b7
gilsunshine <gilsunshine@gmail.com>
2018-03-10 15:45:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return 'This one is on me!'
+  } else if (distance > 2500){
+    return 'No can do.'
+  } else if (distance > 2000){
+    return 'I will gladly take your thirty bucks.'
+  }
+}
+
+function ternaryCheckCity(city){
+  return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.');
+}
+
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous'){
+    return "Thank you so much."
+  } else if (tip === 'not as generous'){
+    return "Thank you."
+  } else {
+    return "Bye."
+  }
+}
