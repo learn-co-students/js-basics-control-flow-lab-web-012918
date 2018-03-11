8097850366548b57b232584979a51bb50de56778
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-03-11 21:49:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code in this file!
+function scuberGreetingForFeet (num) {
+  if (num <= 400) {
+    return('This one is on me!');
+  } else if (num > 2000 && num < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (num > 2500) {
+    return 'No can do.';
+  }
+}
+
+
+function ternaryCheckCity(city) {
+  let response;
+  (city === 'NYC') ? response = "Ok, sounds good." : response = "No go."
+  return response;
+}
+
+function switchOnCharmFromTip (tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      // break;
+    case "not as generous":
+      return "Thank you.";
+      // break;
+    default:
+      return "Bye.";
+  }
+}
