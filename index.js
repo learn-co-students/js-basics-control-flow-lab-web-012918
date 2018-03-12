2e3c883a45183e7f5a95acfae87d70cae9322a35
alin684 <al684@cornell.edu>
2018-03-12 11:23:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your code in this file!
+function scuberGreetingForFeet(ride) {
+  if (ride > 2500) {
+    return 'No can do.'
+  } else if (ride > 2000){
+    return 'I will gladly take your thirty bucks.'
+  } else if (ride <= 400) {
+    return 'This one is on me!'
+  }
+}
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    case 'thanks for everything':
+      return "Bye.";
+  }
+}
